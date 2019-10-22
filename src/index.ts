7b523967265803a72c59809efc846f81d3721b37
Levan Lobjanidze <levalobjanidze@gmail.com>
2019-10-22 11:22:04 +0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Renderer } from './core/renderer';
-export { AssetShader } from './shaders/asset-shader';
+export { AssetShader } from './shaders/asset';
 export { Camera } from './core/camera';
 export { Asset } from './core/asset';
